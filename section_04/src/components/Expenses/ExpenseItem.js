5645212426dcc42cef3f32e6e4a34b3e642c0c19
Ubermensch0608 <melancholyee28@gmail.com>
2022-03-23 23:41:17 +0900

    Docs: state와 useState 내용 추가

--- section_04/src/components/Expenses/ExpenseItem.js
+++ section_04/src/components/Expenses/ExpenseItem.js
@@ -1,15 +1,14 @@
-import React from "react";
+import React, { useState } from "react";
 
 import ExpenseDate from "./ExpenseDate";
 import Card from "../UI/Card";
 import "./ExpenseItem.css";
 
 const ExpenseItem = (props) => {
-  let title = props.title;
+  const [title, setTitle] = useState(props.title);
 
   const changeTitleHandler = () => {
-    title = "updated~";
-    console.log(title);
+    setTitle("Updated!");
   };
 
   return (
